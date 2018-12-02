0551501adf20a1b63697f9f54f8ef5360e1bf397
Chenhua Zhu <zchholmes@gmail.com>
2018-12-01 21:47:54 -0800

    add keras conversion file

---
+++ src/keras_conversion.py
@@ -0,0 +1,18 @@
+import sys
+
+from keras.models import load_model
+
+
+def load_from_model(name_path):
+    model = load_model(name_path)
+    return model
+
+
+def show_summary(model):
+    model.summary()
+
+
+def process_keras_model(name_path):
+    print("calling process_keras_mode...")
+    model = load_from_model(name_path)
+    show_summary(model)
