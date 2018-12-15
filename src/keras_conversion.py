1046e345295db759e85603ae8f30ac96612c5390
Chenhua Zhu <zchholmes@gmail.com>
2018-12-14 18:22:40 -0800

    updates for keras conversion

--- src/keras_conversion.py
+++ src/keras_conversion.py
@@ -1,6 +1,5 @@
 
-from file_utility import remove_temp_file, valid_file, valid_directory
-from keras import Model
+from file_utility import remove_file, valid_file, valid_directory
 
 
 def show_summary(model):
@@ -33,22 +32,28 @@ def load_from_saved_weights(path_topology, path_weights):
     return model
 
 
-def generate_encapsulate_model(model):
+def generate_encapsulate_model(model, output_layer_names=None):
+    """Generate an encapsulate model
+        The new encapsulate model includes:
+        1. model.inputs from original model as enc_model.inputs
+        2. enc_model.outputs has two parts:
+            2.1 transferred identity tensors from original inputs (implemented by Lambda)
+            2.2 tensors from original model
+                2.2.1 default to all tensors from original model
+                2.2.2 based on provided output_layer_names to look up specified layer.output tensors
+    """
     from keras.models import Model
+    from keras.layers import Lambda
+
+    transfer_inputs = list(map(lambda input: Lambda(lambda x: x)(input), model.inputs))
+    if output_layer_names is None:
+        transfer_outputs = list(map(lambda layer: layer.output, model.layers[1:]))
+    else:
+        transfer_outputs = list(map(lambda oln: model.get_layer(oln).output, output_layer_names))
+
     enc_model = Model(
         inputs=model.inputs,
-        # Outputs include ALL input tensors AND output tensors
-        outputs=(model.inputs + list(map(lambda layer: layer.output, model.layers[1:])))
-    )
-    return enc_model
-
-
-def generate_encapsulate_model_with_output_layer_names(model, output_layer_names):
-    from keras.models import Model
-    enc_model = Model(
-        inputs=model.inputs,
-        # Outputs are forced to include ALL input tensors AND required output tensors
-        outputs=(model.inputs + list(map(lambda oln: model.get_layer(oln).output, output_layer_names)))
+        outputs=transfer_inputs+transfer_outputs
     )
     return enc_model
 
@@ -59,15 +64,22 @@ def split_layer_name_list(output_node_names):
 
 def save_enc_model(path_output_dir, enc_model):
     from keras.models import save_model
+    print("Saving enc_model...")
     save_model(enc_model, path_output_dir+"/enc_model.h5")
 
 
 def convert_tfjs(path_output_dir):
     from tensorflowjs.converters.converter import dispatch_keras_h5_to_tensorflowjs_conversion
+    print("Saving converted tfjs model...")
     dispatch_keras_h5_to_tensorflowjs_conversion(
-        path_output_dir+"/enc_model.h5",
+        path_output_dir + "/enc_model.h5",
         path_output_dir
     )
+
+
+def clean_temp_file(path_output_dir):
+    print("Remove enc_model file...")
+    remove_file(path_output_dir + "/enc_model.h5")
 
 
 def preprocess_from_model(path_model, path_output_dir, output_node_names=None):
@@ -79,16 +91,10 @@ def preprocess_from_model(path_model, path_output_dir, output_node_names=None):
         return
 
     model = load_from_saved_model(path_model)
-    if output_node_names is not None:
-        enc_model = generate_encapsulate_model_with_output_layer_names(model, split_layer_name_list(output_node_names))
-    else:
-        enc_model = generate_encapsulate_model(model)
+    enc_model = generate_encapsulate_model(model, split_layer_name_list(output_node_names))
 
     # Generate temp Keras enc_model for further processing
-    print("Saving enc_model...")
     save_enc_model(path_output_dir, enc_model)
-    print("Saving converted tfjs model...")
     convert_tfjs(path_output_dir)
-    print("Remove enc_model file...")
-    remove_temp_file(path_output_dir+"/enc_model.h5")
+    clean_temp_file(path_output_dir)
     print("Mission Complete!!!")
