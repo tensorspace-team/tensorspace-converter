0c744071da839d62dacccff511f824225195f93f
syt123450 <421537891@qq.com>
2018-12-14 18:35:26 -0800

    deprecate export inputs idea

--- src/tfjs/main.js
+++ src/tfjs/main.js
@@ -1,15 +1,12 @@
 const tf = require( '@tensorflow/tfjs' );
 require( '@tensorflow/tfjs-node' );
 const Utils = require( "./utils/Utils" );
-const FunctionalWrapper = require( "./wrapper/Functional" );
-const SequentialWrapper = require( "./wrapper/Sequential" );
+const Wrapper = require( "./wrapper/ModelWrapper" );
 
 let options = process.argv;
 let output_layer_names = undefined;
 let input_path = undefined;
 let output_path = undefined;
-
-let exclude_input = false;
 
 for ( let i = 2; i < options.length - 2; i ++ ) {
 
@@ -19,10 +16,6 @@ for ( let i = 2; i < options.length - 2; i ++ ) {
 	if ( parameters[ 0 ] ===  "--output_layer_names" ) {
 
 		output_layer_names = Utils.getOutputLayerNames( parameters[ 1 ] );
-
-	} else if ( parameters[ 0 ] === "--exclude_input" ) {
-
-		exclude_input = true;
 
 	}
 
@@ -37,21 +30,7 @@ async function encapsulateModel () {
 
 	const model = await tf.loadModel( input_path );
 
-	let modelType = model.toJSON(null, false).class_name;
-
-	console.log( modelType );
-
-	let encModel;
-
-	if ( modelType === "Model" ) {
-
-		encModel = FunctionalWrapper.wrap( model, output_layer_names );
-
-	} else {
-
-		encModel = SequentialWrapper.wrap( model, output_layer_names, exclude_input );
-
-	}
+	let encModel = Wrapper.wrap( model, output_layer_names );
 
 	await encModel.save( output_path );
 
