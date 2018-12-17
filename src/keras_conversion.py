8a25a3b300f55f2106b2541ec8ae60ce179aa2e5
Chenhua Zhu <zchholmes@gmail.com>
2018-12-16 21:56:26 -0800

    updates for keras converter

--- src/keras_conversion.py
+++ src/keras_conversion.py
@@ -2,22 +2,6 @@
 from file_utility import remove_file, valid_file, valid_directory
 
 
-def show_summary(model):
-    model.summary()
-
-
-def show_summary_model(path_model):
-    print("show summary of keras saved model...")
-    model = load_from_saved_model(path_model)
-    show_summary(model)
-
-
-def show_summary_weights(path_topology, path_weights):
-    print("show summary of keras saved topology + weights...")
-    model = load_from_saved_weights(path_topology, path_weights)
-    show_summary(model)
-
-
 def load_from_saved_model(path_model):
     from keras.models import load_model
     model = load_model(path_model)
@@ -43,23 +27,24 @@ def generate_encapsulate_model(model, output_layer_names=None):
                 2.2.2 based on provided output_layer_names to look up specified layer.output tensors
     """
     from keras.models import Model
-    from keras.layers import Lambda
 
-    transfer_inputs = list(map(lambda input: Lambda(lambda x: x)(input), model.inputs))
     if output_layer_names is None:
-        transfer_outputs = list(map(lambda layer: layer.output, model.layers[1:]))
+        transfer_outputs = list(map(lambda layer: layer.output, model.layers[0:]))
     else:
         transfer_outputs = list(map(lambda oln: model.get_layer(oln).output, output_layer_names))
 
     enc_model = Model(
         inputs=model.inputs,
-        outputs=transfer_inputs+transfer_outputs
+        outputs=transfer_outputs
     )
     return enc_model
 
 
 def split_layer_name_list(output_node_names):
-    return output_node_names.split("/")
+    if output_node_names is None:
+        return None
+    else:
+        return output_node_names.split("/")
 
 
 def save_enc_model(path_output_dir, enc_model):
@@ -82,7 +67,50 @@ def clean_temp_file(path_output_dir):
     remove_file(path_output_dir + "/enc_model.h5")
 
 
+def show_summary_model(path_model):
+    """Present model summary by single model file
+
+    Load the model from a single model file, then present model summary
+
+    :param path_model: path to the single model file
+    :return: should not return anything
+    """
+    if (not valid_file(path_model)):
+        print('Aboard converting... INVALID input file.')
+        return
+    print("show summary of keras saved model...")
+    model = load_from_saved_model(path_model)
+    model.summary()
+
+
+def show_summary_weights(path_topology, path_weights):
+    """Present model summary by model topology and weights
+
+    Load the model from topology and weights, then present model summary
+
+    :param path_topology: path to model topology file
+    :param path_weights: path to model weights file
+    :return: should not return anything
+    """
+    if (not valid_file(path_topology)):
+        print('Aboard converting... INVALID model topology file.')
+        return
+    if (not valid_file(path_weights)):
+        print('Aboard converting... INVALID model weights file.')
+        return
+    print("show summary of keras saved topology + weights...")
+    model = load_from_saved_weights(path_topology, path_weights)
+    model.summary()
+
+
 def preprocess_from_model(path_model, path_output_dir, output_node_names=None):
+    """Preprocess a model built by Keras (from single .h5 file)
+
+    :param path_model:
+    :param path_output_dir:
+    :param output_node_names:
+    :return: should not return anything
+    """
     if (not valid_file(path_model)):
         print('Aboard converting... INVALID input file.')
         return
@@ -91,6 +119,35 @@ def preprocess_from_model(path_model, path_output_dir, output_node_names=None):
         return
 
     model = load_from_saved_model(path_model)
+    enc_model = generate_encapsulate_model(model, split_layer_name_list(output_node_names))
+
+    # Generate temp Keras enc_model for further processing
+    save_enc_model(path_output_dir, enc_model)
+    convert_tfjs(path_output_dir)
+    clean_temp_file(path_output_dir)
+    print("Mission Complete!!!")
+
+
+def preprocess_from_weights(path_topology, path_weights, path_output_dir, output_node_names=None):
+    """""Preprocess a model built by Keras (from topology+weights)
+
+    :param path_topology:
+    :param path_weights:
+    :param path_output_dir:
+    :param output_node_names:
+    :return:
+    """
+    if (not valid_file(path_topology)):
+        print('Aboard converting... INVALID model topology file.')
+        return
+    if (not valid_file(path_weights)):
+        print('Aboard converting... INVALID model weights file.')
+        return
+    if (not valid_directory(path_output_dir)):
+        print('Aboard converting... INVALID output directory.')
+        return
+
+    model = load_from_saved_weights(path_topology, path_weights)
     enc_model = generate_encapsulate_model(model, split_layer_name_list(output_node_names))
 
     # Generate temp Keras enc_model for further processing
--- src/tfjs_conversion.py
+++ src/tfjs_conversion.py
@@ -4,13 +4,17 @@ def main():
     # subprocess.call(["ls", "-l"])
     # subprocess.call(["echo", "Hello World~"])
     subprocess.check_call(["echo", "Hello World~"])
-    subprocess.check_call(["pwd"])
+    subprocess.check_call(["pwd"], shell=True)
 
-    main_js_path = "/Users/zchholmes/PycharmProjects/tensorspacejs/src/tfjs/main.js"
-    output_names = "--output_layer_names='myPadding/myConv1/myMaxPooling1/myConv2/myMaxPooling2/myDense1/myDense2/myDense3'"
-    path_input = "/Users/zchholmes/PycharmProjects/tensorspacejs/src/tfjs/test/sequential/mnist.json"
-    path_output = "/Users/zchholmes/PycharmProjects/tensorspacejs/src/tfjs/test"
-    subprocess.check_call(["node", main_js_path, output_names, path_input, path_output])
+    # main_js_path = "/Users/zchholmes/PycharmProjects/tensorspacejs/src/tfjs/main.js"
+    # output_names = "--output_layer_names='myPadding/myConv1/myMaxPooling1/myConv2/myMaxPooling2/myDense1/myDense2/myDense3'"
+    # path_input = "/Users/zchholmes/PycharmProjects/tensorspacejs/test/tfjs/model/mnist.json"
+    # path_output = "/Users/zchholmes/PycharmProjects/tensorspacejs/test/tfjs"
+    # subprocess.check_call(["node", main_js_path, output_names, path_input, path_output])
+
+    subprocess.check_call(['sh', './test/tfjs/model/test'])
+
+
 
 
 
