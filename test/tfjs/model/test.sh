e1c9267f8a5c7e532570d55e5ba5f8daa0001fa5
syt123450 <421537891@qq.com>
2018-12-26 00:27:39 -0800

    modify tfjs test script with new entry point

---
+++ test/tfjs/model/test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+python ./src/tsp_converters.py \
+--input_type="tfjs" \
+--output_node_names='myPadding/myConv1/myMaxPooling1/myConv2/myMaxPooling2/myDense1/myDense2/myDense3' \
+./test/tfjs/model/mnist.json \
+./test/tfjs
---
+++ test/tfjs/nonames/test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+python ./src/tsp_converters.py \
+--input_type="tfjs" \
+./test/tfjs/sequential/mnist.json \
+./test/tfjs
