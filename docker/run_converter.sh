b19cb58f658add807d7916072917bb30ddb13b12
botime <25629006+BoTime@users.noreply.github.com>
2019-03-18 00:17:43 -0700

    Disable docker detached mode

--- docker/run_converter.sh
+++ docker/run_converter.sh
@@ -10,11 +10,14 @@ fi
 
 echo "Start converting model..."
 
-docker run -d -it \
+docker rm -f ts-converter
+
+docker run -it \
     --mount type=bind,source="$(pwd)"/$base_dir,target=/data \
-    --name ts-convert \
+    --name ts-converter \
     tensorspacejs
 
-docker logs ts-convert
+docker logs ts-converter
+
 echo "Model is saved to output folder"
 echo "Finished converting model!"
