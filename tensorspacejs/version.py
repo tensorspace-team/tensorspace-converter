565d2783064ec84da79e6606f434c82826bcdec8
Pei Jia <jiapei@longervision.com>
2020-04-17 02:03:19 -0700

    Required package version is updated to newer ones.

--- tensorspacejs/version.py
+++ tensorspacejs/version.py
@@ -1,10 +1,10 @@
 # tensorspacejs version
-version = '0.2.0'
+version = '0.6.1'
 # dependencies version
 python_version = '3.6'
 node_version = '11.3+'
 npm_version = '6.5+'
-tensorflow_version = '1.12.0'
-keras_version = '2.2.2'
+tensorflow_version = '2.0.0+'
+keras_version = '2.2.2+'
 # TODO: Migrate to tfjs-converter 1.0
-tensorflowjs_version = '0.8.0'
+tensorflowjs_version = '0.8.0+'
